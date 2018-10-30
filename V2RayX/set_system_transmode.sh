9bea7211efc282acc2781930bcd247e3b41abce9
leslie <hushuangsoutheastasia@gmail.com>
2018-10-30 09:57:13 +0800

    透明模式更新为无墙dns, 修复台式mac dns设置

--- V2RayX/set_system_transmode.sh
+++ V2RayX/set_system_transmode.sh
@@ -6,7 +6,7 @@
 #  Created by leslie on 10/27/18.
 #  Copyright © 2018 Project V2Ray. All rights reserved.
 
-sudo networksetup -setdnsservers Wi-Fi 8.8.8.8
-sudo networksetup -setdnsservers Ethernet 8.8.8.8
+sudo networksetup -setdnsservers Wi-Fi 120.78.224.69
+sudo networksetup -setdnsservers Ethernet 120.78.224.69
 
 echo done
